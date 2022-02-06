283a226c2b55c5186db54abf834ce6a391a081ae
okanbahadirsoygur <okanbahadirsoygur@gmail.com>
2022-02-07 00:27:40 +0300

    admin/settings update özelliği eklendi

--- src/main/java/com/example/dort/Controller/AdminController.java
+++ src/main/java/com/example/dort/Controller/AdminController.java
@@ -5,6 +5,7 @@ import com.example.dort.repos.SettingsRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,34 @@ public class AdminController {
         modelAndView.setViewName("admin/settings");
 
         return modelAndView;
+
+    }
+
+
+    /**
+     * <p>
+     * Bu sayfaya sadece post metodu ile erişilebilir.
+     * /admin/settings sayfasından güncellenip gönderilen veriler bu sayfaya post ile gelir.
+     * </p>
+     *
+     * <p>
+     * RequestParam annotation'ı ile gelen json verilerini, tanımladığımız değişken isimleri ile eşleyip yakalayabiliyoruz.
+     * </p>
+     */
+    @PostMapping("/admin/settings/update")
+    public RedirectView settings_update(@RequestParam String LOGO_URL, String FOOTER_DESC, String FOOTER_LICANCE, String LOGO_TEXT, String GITHUB_LINK){
+
+
+        settingsRepos.setSettingsByKey("LOGO_URL",LOGO_URL);
+        settingsRepos.setSettingsByKey("LOGO_TEXT",LOGO_TEXT);
+        settingsRepos.setSettingsByKey("FOOTER_DESC",FOOTER_DESC);
+        settingsRepos.setSettingsByKey("FOOTER_LICANCE",FOOTER_LICANCE);
+        settingsRepos.setSettingsByKey("GITHUB_LINK",GITHUB_LINK);
+
+        //önceki sayfaya yeniden yönlendirelim.
+        return new RedirectView("/admin/settings");
+
+
 
     }
 
--- src/main/java/com/example/dort/repos/SettingsRepos.java
+++ src/main/java/com/example/dort/repos/SettingsRepos.java
@@ -2,12 +2,26 @@ package com.example.dort.repos;
 
 import com.example.dort.Entities.Settings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Repository
 public interface SettingsRepos extends JpaRepository<Settings, Long> {
 
     @Query("select s from Settings s where s.key = ?1")
     Settings getSettingsByKey(String key);
+
+    /**
+     * <p>
+     * Update işlemini gerçekleştirebilmek için Modifying, Transactional annotations'ları tanımlanmak zorundadır.
+     * Query türü nativeQuery olmalıdır.
+     * </p>
+     */
+    @Modifying
+    @Query(value = "update Settings s set s.value = ?2 where s.key = ?1", nativeQuery = true)
+    void setSettingsByKey( String key, String value);
 }
